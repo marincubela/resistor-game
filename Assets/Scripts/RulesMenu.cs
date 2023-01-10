1d0241260515d035274a924a3d7c02699aefcfbc
AndreaKristo <andrea.kristo@protel.net>
2023-01-10 03:27:14 +0100

    Change background

--- Assets/Scripts/RulesMenu.cs
+++ Assets/Scripts/RulesMenu.cs
@@ -7,6 +7,6 @@ public class RulesMenu : MonoBehaviour
 {
    public void RulesGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
